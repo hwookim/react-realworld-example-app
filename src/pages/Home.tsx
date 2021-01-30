ba1c2bceaec74240b70f3499f4b2bd3e9e772e04
hwookim <gusdn9754@gmail.com>
2021-01-30 22:15:55 +0900

    feat: 배너 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Home(): JSX.Element {
-  return <div>Home</div>;
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1 className="logo-font">home</h1>
+          <p>A place to share your knowledge.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
